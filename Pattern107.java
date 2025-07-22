31480b85a372c067f0f64cd6dc79f2a7735819c4
swapnil <swapnilghayal007@gmail.com>
2025-07-23 01:25:56 +0530

    Day 3

---
+++ Pattern107.java
@@ -0,0 +1,41 @@
+public class Pattern107
+{
+
+		public static void main(String args[])
+		{
+
+			int n=10;
+			for(int i=1; i<=n; i++ )
+			{
+				for(int j=n; j>=i; j-- )
+				{
+	
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+
+
+
+
+
+
+
+
+
+
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+}
